f0031dd8621712a0f23f03a71e78725f9aac8661
Evgen-ios <jack38irk@icloud.com>
2020-10-21 20:12:33 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Evgeniy Goncharov on 21.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
